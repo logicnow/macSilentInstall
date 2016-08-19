a1d3ea374254b5f71861fc2f66d02cead9f0960a
Brian J Best <brian@bestmacs.com>
2016-08-19 13:54:12 -0500

    Version 1.2
    Updated to better handle Client and Site registrations, improve user
    dialogs, error handling

--- macSilentInstall.sh
+++ macSilentInstall.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# LOGICnow MAX Remote Management OS X Agent Silent Deployment
-# Requires OS X Agent 1.5.1 or better
+# LOGICnow MAX Remote Management OS X Agent Silent Install and Registration
+# Requires OS X Agent 1.5.3 or better
 
 # by Brian J Best, Apple Strategist
 # v1.0 initial release
 # v1.1 properly dismounts the disk image, checks for root privs
+# v1.1.1 uses OS X Agent 1.5.3 and -C -S to create client site as needed
+# v1.2 does a little song and dance with -C -S -c -s to get good enrollment
 
 # VARIABLES
 # you can specify variables as command-line arguments when running the script
@@ -41,14 +43,14 @@ if [ "$rmUsername" == "-?" ]; then
     echo "Run the script with the 4 required variables."
     echo "Example: /path/to/macSilentInstall.sh username password client site [--registeronly]"
     echo ""
-    echo "'username' is the username (likely email address) you use to log in the MAX Remote Management Dashboard"
+    echo "'username' is the username (ideally the Agent Key) you use to log in the MAX Remote Management Dashboard"
     echo "'password' is the corresponding password for the username, which will appear in clear text in your command. See below."
     echo "'client' is the client name you wish to assign to the computer running this script"
     echo "'site' is the site of the above client that you wish to assign to the computer running this script"
     echo "Adding '--registeronly' will skip the download/install and simply register the agent"
     echo ""
     echo "Bear that in mind with your deployment method your password will be sent in clear text."
-    echo "Please use a Standard user account to protect your dashboard, read this for a safety tip:"
+    echo "Please consider using the Agent Key user account with Dashboard Access disabled, read this for a safety tip:"
     echo "http://www.allthingsmax.com/2011/11/security-best-practices-in-max.html"
     exit 0
 fi
@@ -76,7 +78,7 @@ fi
 
 if [ ${regOnly:-0} -eq 0 ]; then
 # download the agent installer
-curl -o "/private/tmp/osxagent-$randomID.dmg" "https://www.mac-msp.com/osxagent151.dmg"
+curl -o "/private/tmp/osxagent-$randomID.dmg" "https://www.mac-msp.com/osxagent153.dmg"
 #TODO - may need to replace this download link at some point
 
 # mount the DMG, specify the mountpoint since the DMG volume name might change later
@@ -102,10 +104,36 @@ fi
 
 fi #regOnly check
 
-# enroll in RM - little oddity requires wd to be parent folder
+# enroll in RM
 if [ -e /usr/local/rmmagent/rmmagentd ]; then
-    cd /usr/local/rmmagent; ./rmmagentd -q -u "$rmUsername" -p "$rmPass" -c "$rmClient" -s "$rmSite"
-    # TODO - change to -C and -S when Agent 1.5.2 is released
+    # little oddity requires wd to be parent folder
+    cd /usr/local/rmmagent
+    ./rmmagentd -q -u "$rmUsername" -p "$rmPass" -C "$rmClient" -S "$rmSite"
+    if [ $? -ne 0 ]; then
+        # let's do the registration dance
+        echo "Registration could not create client and site. Trying existing."
+        #no joy in creating client/site, maybe already there?
+        regAtt2=`./rmmagentd -q -u "$rmUsername" -p "$rmPass" -c "$rmClient" -s "$rmSite"`
+        if [ $? -ne 0 ]; then
+            #nope.
+            case "$regAtt2" in
+            'Duplicate device name for Client/Site combination')
+                echo "Registration failed because a device with this name already exists in the Dashboard. Remove it and try again."
+            ;;
+            'Agent is already registered')
+            	echo "This computer appears to be in the Dashboard already. Please check for it there."
+            ;;
+            *)
+                # try creating site only
+                echo "Registration doesn't have existing client and site. Trying to create Site."
+                ./rmmagentd -q -u "$rmUsername" -p "$rmPass" -c "$rmClient" -S "$rmSite"
+                if [ $? -ne 0 ]; then
+                    #fail.
+                    echo "Registration failed. You may need to register this computer manually."
+                fi
+            esac
+        fi
+    fi
 else
     if [ ${regOnly:-0} -eq 1 ]; then
         echo "OS X Agent does not appear to be installed. Please re-run without --registeronly."
@@ -116,8 +144,9 @@ else
 fi
 
 # sanity check 
-if [ $? -ne 0 ]; then
-    echo "Unable to register the computer. Please check the variables and try again."
+regCheck=`grep 'Mac OSX agent is registered successfully.' /usr/local/rmmagent/log/rmmagentd.log`
+if [ "$regCheck" == "" ]; then
+    echo "Something went wrong. Please check the variables and try again or register manually."
     exit 2
 else
     echo "Everything appears to have worked. Please check the RM Dashboard for the computer record."
